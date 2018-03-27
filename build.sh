2091e941f8aa7177f8a5482f06e08b41a74e14fa
grd <greg.dooper@farmersedge.ca>
2018-03-27 14:24:10 -0600

    Install Kafka DS Geoserver plugin

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-t <tag>] [-g <geomesa.version>] [-a <accumulo.version>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-t <tag>] [-k <kafka.version>] [-g <geomesa.version>] [-a <accumulo.version>]" 1>&2; exit 1; }
 
 ACCUMULO_VERSION=1.7.3
 GEOMESA_VERSION=2.0.0-m.1
+KAFKA_VERSION=10
 
-while getopts ":t:g:a:" o; do
+while getopts ":t:k:g:a:" o; do
     case "${o}" in
         a)
             ACCUMULO_VERSION=${OPTARG}
             ;;
         g)
             GEOMESA_VERSION=${OPTARG}
+            ;;
+        k)
+            KAFKA_VERSION=${OPTARG}
             ;;
         t)
             TAG=${OPTARG}
@@ -27,9 +31,10 @@ if [ -z "$TAG" ]; then TAG=geomesa-${GEOMESA_VERSION}-accumulo-${ACCUMULO_VERSIO
 
 if [[ $ACCUMULO_VERSION == 1.8* ]]; then THRIFT_VERSION=0.9.3; else THRIFT_VERSION=0.9.2; fi
 
-echo "GEOMESA_VERSION = ${GEOMESA_VERSION}"
 echo "ACCUMULO_VERSION = ${ACCUMULO_VERSION}"
+echo "GEOMESA_VERSION = ${GEOMESA_VERSION}"
+echo "KAFKA_VERSION = ${KAFKA_VERSION}"
 echo "TAG = ${TAG}"
 
-export ACCUMULO_VERSION GEOMESA_VERSION TAG THRIFT_VERSION
+export ACCUMULO_VERSION GEOMESA_VERSION KAFKA_VERSION TAG THRIFT_VERSION
 make publish
