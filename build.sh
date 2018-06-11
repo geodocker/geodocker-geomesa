56c4213e6b536a759c7ec1274581b2e2367f7624
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2018-06-11 17:05:13 -0400

    Updating to GeoMesa version 2.0.2 and Accumulo 1.9.1

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 usage() { echo "Usage: $0 [-t <tag>] [-g <geomesa.version>] [-a <accumulo.version>]" 1>&2; exit 1; }
 
-ACCUMULO_VERSION=1.9.0
-GEOMESA_VERSION=2.0.1
+ACCUMULO_VERSION=1.9.1
+GEOMESA_VERSION=2.0.2
+THRIFT_VERSION=0.9.3
 
 while getopts ":t:g:a:" o; do
     case "${o}" in
@@ -24,8 +25,6 @@ done
 shift $((OPTIND-1))
 
 if [ -z "$TAG" ]; then TAG=geomesa-${GEOMESA_VERSION}-accumulo-${ACCUMULO_VERSION}; fi
-
-if [[ $ACCUMULO_VERSION == 1.8* ]]; then THRIFT_VERSION=0.9.3; else THRIFT_VERSION=0.9.2; fi
 
 echo "GEOMESA_VERSION = ${GEOMESA_VERSION}"
 echo "ACCUMULO_VERSION = ${ACCUMULO_VERSION}"
